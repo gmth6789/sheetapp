82b6dd4a51c57ed9f83f9ac7035582785947b840
gmth6789 <gmth.6789@gmail.com>
2023-11-13 11:03:45 +0700

    origin

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const { resolve } = require('path');
+const cors = require('cors')
 
 const app = express();
-const port = 3010;
+const port = 9000;
 
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+app.use(cors())
+app.use(express.json())
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
