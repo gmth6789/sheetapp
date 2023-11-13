bf52d3990184d0283599ff062e58a699e58e410d
gmth6789 <gmth.6789@gmail.com>
2023-11-13 11:15:45 +0700

    origin

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+// const dotenv = require('dotenv/config');
+
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const line = require('@line/bot-sdk');
+
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
 
 const app = express();
 const port = 9000;
 
-app.use(cors())
-app.use(express.json())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
