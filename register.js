887cde4fd065c9a198b6d389acb2f005e6d0b954
gmth6789 <gmth.6789@gmail.com>
2023-11-13 12:07:14 +0700

    origin

--- register.js
+++ register.js
@@ -5,9 +5,9 @@ const Register = (req,res) => {
         username : req.body.username,
         password : req.body.password
     }
-    let url = 'https://sheet.best/api/sheets/1eb91623-5f06-4dd7-b1f5-4c006dae0e15'
+    let url = ''
     let header = {
-        'Api-Key': 'mopvpOmi4!IYreO@b61kkEieQX6Ru@0QL$avC7Nxb-#B8tBKmTbp848YQV9Qc%s4'
+        'Api-Key': ''
     }
     try {
         const result = axios.post(url,obj,header)
